50c1f2acdd69a1dac5e993d810b0d4d402be653c
Egor Nikitin yzk-jssc <egonik1337@gmail.com>
2023-04-25 00:24:20 +0400

    build(mapbox): added and used token

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import VueMapboxTs from "vue-mapbox-ts";
+
 
 createApp(App).mount('#app')
+
+
+const app = createApp(App);
+
+app.use(VueMapboxTs)
