c0548bb0382bf1e7348ffb7871853e1dd8b79ea6
Egor Nikitin yzk-jssc <egonik1337@gmail.com>
2023-04-27 00:16:15 +0400

    build(store): definition pinia

--- front/src/main.ts
+++ front/src/main.ts
@@ -2,11 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import VueMapboxTs from "vue-mapbox-ts";
-
-
-createApp(App).mount('#app')
+import { createPinia } from 'pinia'
 
 
 const app = createApp(App);
-
 app.use(VueMapboxTs)
+createApp(App).use(createPinia()).mount('#app');
