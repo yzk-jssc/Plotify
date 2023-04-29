69b0b4cea6b4e86e154eec3c3687426914b5ce89
Egor Nikitin yzk-jssc <egonik1337@gmail.com>
2023-04-29 17:46:37 +0400

    build(mapbox): added package for drawing
    - due to refactoring from vue mapbox to default mapbox this package is require

--- front/src/store/plots.ts
+++ front/src/store/plots.ts
@@ -1,26 +1,31 @@
 import { defineStore } from "pinia";
 import * as plotApi from '../service/plot.service'
-import { Plot } from "../types/create";
+import { PlotRequestData } from "../types/plots.service";
 
 export const usePlotsStore = defineStore('plots', {
   state: () => ({
     plots: [] as GeoJSON.Feature[]
   }),
   actions: {
-    createNewPlotLocally(plot: GeoJSON.Feature[]){
-      this.plots = [...this.plots, ...plot]
+    createNewPlotLocally(plot: GeoJSON.Feature){
+      this.plots = [plot, ...this.plots]
     },
-    // workaround
-    // do types later
-    createNewPlot(plot: any){
-      this.createNewPlotLocally(plot)
-      if(typeof plot[0].id === 'string'){
-        const data: Plot = {
-          featureId: plot[0].id,
-          plotcoordinations: plot[0].geometry.coordinates
+    createNewPlot(plot: GeoJSON.Feature, zoom: number){
+      if(typeof plot.id === 'string'){
+        const data:PlotRequestData = {
+          featureId: plot.id,
+          //evading float numbers
+          zoom: Math.floor(zoom),
+          feature: plot
         }
         plotApi.createNewPlot(data)
+        this.createNewPlotLocally(plot)
       }
+    },
+    async getAllPlots(): Promise<GeoJSON.Feature[]>{
+      const allPlots = await plotApi.getAllPlots()
+      this.plots = [...allPlots]
+      return this.plots
     }
   }
 })
